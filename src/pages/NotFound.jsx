0fe7482dc71f0e95c4fd337397735c92be81a6fa
roy-hendry <royjairusraj@hotmail.com>
2022-06-30 09:00:48 +0100

    Pages And Routes - improved NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,21 @@
+import { FaHome } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
 function NotFound() {
-  return <div>
-  <h1 className="text-6xl mb-4">Error #404 page not found</h1>
-</div>;
+  return <div className="hero">
+    <div className="text-center hero-content">
+      <div className="max-w-lg">
+        <h1 className="text-8xl font-bold mb-8">
+          Ooops!
+        </h1>
+        <p1 className="text-5xl mb-8">404 page not found!</p1>
+        <Link to="/" className="btn btn-primary btn-lg">
+          <FaHome className="mr-2"/>
+          Back To Home
+        </Link>
+      </div>
+    </div>
+  </div>;
 }
 
 export default NotFound;
