48a2acdd08dcf4118903f4c67e5aed216b96730f
roy-hendry <royjairusraj@hotmail.com>
2022-06-30 08:49:33 +0100

    Pages And Routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+function About() {
+  return <div>
+    <h1 className="text-6xl mb-4">Github Finder</h1>
+        <p>What's poppin? Brand new whip, just hopped in.</p>
+  </div>;
+}
+
+export default About;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+function NotFound() {
+  return <div>
+  <h1 className="text-6xl mb-4">Error #404 page not found</h1>
+</div>;
+}
+
+export default NotFound;
