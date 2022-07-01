6b1e47d8dbd35ccbfd0737c16dc0d508826f1366
roy-hendry <royjairusraj@hotmail.com>
2022-07-01 08:06:00 +0100

    Added a few comments for clarity

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ function App() {
         <Router>
             <div className="flex flex-col justify-between h-screen">
                 <Navbar />
-
                 <main className="container mx-auto px-3 pb-12">
-                    <Routes>
+                    <Routes
+                    //Each component when accessed will bring you to the following path
+                    >
                         <Route path="/" element={<Home />} />
                         <Route path="/about" element={<About />} />
                         <Route path="/notfound" element={<NotFound />} />
+
                         <Route path="/*" element={<NotFound />} />
                     </Routes>
                 </main>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,17 @@
 import { FaHome } from "react-icons/fa"
 import { Link } from "react-router-dom"
 
+//This page will help with the syntax of tailwindcss at the time of this comment we are using tailwindcss v3.1.4
+//https://tailwindcss.com/docs/padding
+
 function NotFound() {
   return <div className="hero">
     <div className="text-center hero-content">
       <div className="max-w-lg">
-        <h1 className="text-8xl font-bold mb-8">
-          Ooops!
-        </h1>
-        <p1 className="text-5xl mb-8">404 page not found!</p1>
+        <h1 className="text-8xl font-bold mb-8">Ooops!</h1>
+        <div className="pb-6">
+          <p1 className="text-5xl mb-8">404 - Page not found!</p1>
+        </div>
         <Link to="/" className="btn btn-primary btn-lg">
           <FaHome className="mr-2"/>
           Back To Home
