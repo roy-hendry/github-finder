e89576cc6df66570e800c1b7ff95508862e0db9e
roy-hendry <royjairusraj@hotmail.com>
2022-07-04 19:01:52 +0100

    Display Users

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,6 @@
 import {FaGithub} from "react-icons/fa"
 import {Link} from "react-router-dom"
 import PropTypes from "prop-types"
-import { Container } from "postcss"
 
 function Navbar({title}) {
     return (
