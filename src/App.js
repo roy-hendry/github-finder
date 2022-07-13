0e0606b4317aa35d6a7a2ac74a95c523e4651622
roy-hendry <royjairusraj@hotmail.com>
2022-07-13 07:51:38 +0100

    Alert Context & Reducer

--- src/App.js
+++ src/App.js
@@ -5,28 +5,34 @@ import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 function App() {
     return (
         <GithubProvider>
-            <Router>
-                <div className="flex flex-col justify-between h-screen">
-                    <Navbar />
-                    <main className="container mx-auto px-3 pb-12">
-                        <Routes
-                        //Each component when accessed will bring you to the following path
-                        >
-                            <Route path="/" element={<Home />} />
-                            <Route path="/about" element={<About />} />
-                            <Route path="/notfound" element={<NotFound />} />
-                            {/*if you try to go to any of the pages that don't
+            <AlertProvider>
+                <Router>
+                    <div className="flex flex-col justify-between h-screen">
+                        <Navbar />
+                        <main className="container mx-auto px-3 pb-12">
+                            <Routes
+                            //Each component when accessed will bring you to the following path
+                            >
+                                <Route path="/" element={<Home />} />
+                                <Route path="/about" element={<About />} />
+                                <Route
+                                    path="/notfound"
+                                    element={<NotFound />}
+                                />
+                                {/*if you try to go to any of the pages that don't
                             exist you'll get sent to the NotFound page */}
-                            <Route path="/*" element={<NotFound />} />
-                        </Routes>
-                    </main>
-                    <Footer />
-                </div>
-            </Router>
+                                <Route path="/*" element={<NotFound />} />
+                            </Routes>
+                        </main>
+                        <Footer />
+                    </div>
+                </Router>
+            </AlertProvider>
         </GithubProvider>
     );
 }
